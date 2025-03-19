284226f5ccac170d516456b3a037af522e47a896
Peter Fänge <pellefange@gmail.com>
2025-03-19 11:25:00 +0100

    updated geoLocationApi.tsx

--- src/services/geoLocationApi.tsx
+++ src/services/geoLocationApi.tsx
@@ -1,17 +1,23 @@
-const fetchLocationData = async () => {
+export const fetchLocationData = async () => {
 
   const api_key = "0446888e31c3f0f934343d993bca85e5";
-  const api_url: string = `https://geokeo.com/geocode/v1/search.php?q=empire+state+building&api=${api_key}`;
+  const api_url: string = `https://geokeo.com/geocode/v1/search.php?q=helsingborg&api=${api_key}`;
 
   try {
     const response = await fetch(api_url);
     const locationData = await response.json();
-    console.log("Longitute: " + locationData.results[0].geometry.location.lng);
-    console.log("Latitude: " + locationData.results[0].geometry.location.lat);
-    console.log("Address: " + locationData.results[0].formatted_address);
+
+    const lng: number = locationData.results[0].geometry.location.lng
+    const lat: number = locationData.results[0].geometry.location.lat
+    const address: string = locationData.results[0].formatted_address
+
+    // console.log("Longitute: " + lng);
+    // console.log("Latitude: " + lat);
+    // console.log("Address: " + address);
+
+    return {lng, lat, address}; 
+
   } catch (error) {
     console.log("Could not fetch data from API" + "-" +  error);
   }
 };
-
-fetchLocationData();
