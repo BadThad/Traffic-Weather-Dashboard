d383d3375814a266117a1675217291214b7c6db8
Peter Fänge <pellefange@gmail.com>
2025-03-25 09:47:25 +0100

    made some changes in structure and naming

--- src/services/geoLocationApi.tsx
+++ src/services/geoLocationApi.tsx
@@ -1,21 +1,28 @@
+import { Coordinates } from "../types";
+
 const api_key = "0446888e31c3f0f934343d993bca85e5";
 
-export const fetchCoordinates = async (query: string) => {
-  const response = await fetch(
-    `https://geokeo.com/geocode/v1/search.php?q=${encodeURIComponent(query)}&api=${api_key}`
-  );
+export const fetchCoordinates = async (query: string): Promise<Coordinates> => {
+  try {
+    const response = await fetch(
+      `https://geokeo.com/geocode/v1/search.php?q=${encodeURIComponent(query)}&api=${api_key}`
+    );
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch geolocation data.");
+    if (!response.ok) {
+      throw new Error("Failed to fetch geolocation data.");
+    }
+
+    const data = await response.json();
+    const location = data.results?.[0]?.geometry?.location;
+    if (!location) {
+      throw new Error("Location not found.");
+    }
+
+    return {
+      latitude: location.lat,
+      longitude: location.lng,
+    };
+  } catch (error) {
+    throw new Error(error.message);
   }
-
-  const data = await response.json();
-  if (data.result.length === 0) {
-    throw new Error("No results found.");
-  }
-
-  return {
-    lat: data.results[0].geometry.location.lat,
-    lng: data.results[0].geometry.location.lng,
-  };
 };
