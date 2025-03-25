8d1318b9b43ad75d782b51e1e6bc30fd905b5d6c
Peter Fänge <pellefange@gmail.com>
2025-03-25 09:46:47 +0100

    some changes to test build, not complete

--- src/components/trafficSituation.tsx
+++ src/components/trafficSituation.tsx
@@ -1,30 +1,46 @@
 import "./trafficSituationStyles.css"
+import { useEffect } from "react";
 import { useSituationsStore } from "../stores/situationsStore";
 import { useSearchStore } from "../stores/searchStore";
-import { fetchCoordinates } from "../services/geoLocationApi";
+import fetchTrafikverketAPI from "../services/trafficSituationApi";
 
-function TrafficSituation() {
+const TrafficSituation: React.FC = () => {
 
-    const coord = useSearchStore((state) => state.coordinates);
+  const coordinates = useSearchStore((state) => state.coordinates);
+  const where = useSituationsStore((state) => state.where);
+  const what = useSituationsStore((state) => state.what);
+  const why = useSituationsStore((state) => state.why);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-const { fetchSituationAsync } =
-useSituationsStore();
-    
+    useEffect(() => {
+        if (!coordinates) return;
 
-const where = useSituationsStore((state) => state.where);
-const what = useSituationsStore((state) => state.what);
-const why = useSituationsStore((state) => state.why);
+        const getSituation = async () => {
+            setLoading(true);
+            setError(null);
 
-    return (
-        <div className="componentContainer">
-            <p>{where}</p>  {/*VAR ÄR SITUATIONEN*/}
-            <p>{what}</p>   {/*VAD FÖR SITUATION*/}
-            <p>{why}</p>    {/*BESKRIVNING AV SITUATIONEN*/}   
-            <button onClick={() => fetchSituationAsync(coord.lat, coord.lng)}>
-              KLICK FOR SITUATION
-            </button>       
-        </div>
-    );
+            try {
+                const situation = await fetchTrafikverketAPI(coordinates.lat, coordinates.lng);
+                set where(where)
+            }
+        }
+    }, [coordinates, ])
+  
+  const { fetchSituationAsync } = useSituationsStore();
+
+  return (
+    <div className="componentContainer">
+      <p>{where}</p> {/*VAR ÄR SITUATIONEN*/}
+      <p>{what}</p> {/*VAD FÖR SITUATION*/}
+      <p>{why}</p> {/*BESKRIVNING AV SITUATIONEN*/}
+      <button
+        onClick={() => fetchSituationAsync(coordinates.lat, coordinates.lng)}
+      >
+        KLICK FOR SITUATION
+      </button>
+    </div>
+  );
 };
 
 export default TrafficSituation;
